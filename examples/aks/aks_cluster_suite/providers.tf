92a2f1b86ec6f2c5bc876036eebe1c37a2d6fc90
Elijah Abdulrahman <elijah@cast.ai>
2024-06-05 12:49:35 -0700

    feat: TF Example for all toggleable options

---
+++ examples/aks/aks_cluster_suite/providers.tf
@@ -0,0 +1,21 @@
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {
+  tenant_id = data.azurerm_subscription.current.tenant_id
+}
+
+provider "castai" {
+  api_url   = var.castai_api_url
+  api_token = var.castai_api_token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = azurerm_kubernetes_cluster.this.kube_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
+  }
+}
