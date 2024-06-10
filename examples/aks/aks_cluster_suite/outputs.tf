f5bcfbeb9b0c9816f6cc9c5e66a12d55af9c726e
Elijah Abdulrahman <elijah@cast.ai>
2024-06-10 16:07:18 -0700

    fix: castai_aks_cluster timing out in RO mode

--- examples/aks/aks_cluster_suite/outputs.tf
+++ examples/aks/aks_cluster_suite/outputs.tf
@@ -1,8 +1,3 @@
-output "cluster_id" {
-  value = castai_aks_cluster.this.id
-  description = "CAST AI cluster ID."
-}
-
 output "cluster_token" {
   value       = var.castai_api_token
   description = "CAST AI cluster token used by Castware to atuhenticate to Mothership."
