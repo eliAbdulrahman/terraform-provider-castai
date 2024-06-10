9d7b9d32cbc466f6ae07b9a4faf47404659b1094
Elijah Abdulrahman <elijah@cast.ai>
2024-06-10 16:07:18 -0700

    fix: castai_aks_cluster timing out in RO mode

--- examples/aks/aks_cluster_suite/castai.tf
+++ examples/aks/aks_cluster_suite/castai.tf
@@ -13,6 +13,7 @@ resource "castai_aks_cluster" "this" {
 
 }
 
+# Setup readonly agent
 resource "helm_release" "castai_agent" {
   name             = "castai-agent"
   repository       = "https://castai.github.io/helm-charts"
@@ -44,10 +45,24 @@ resource "helm_release" "castai_agent" {
   }
 }
 
+resource "castai_aks_cluster" "this" {
+  count           = var.readonly ? 0 : 1
+  name            = var.cluster_name
+
+  region          = var.cluster_region
+  subscription_id = data.azurerm_subscription.current.subscription_id
+  tenant_id       = data.azurerm_subscription.current.tenant_id
+  client_id       = var.readonly ? null : azuread_application.castai.application_id
+  client_secret   = var.readonly ? null : azuread_application_password.castai.value
+
+  node_resource_group        = azurerm_kubernetes_cluster.this.node_resource_group
+  delete_nodes_on_disconnect = var.delete_nodes_on_disconnect
+}
+
+
 resource "castai_node_configuration" "default" {
   count          = var.readonly ? 0 : 1
-  cluster_id     = castai_aks_cluster.this.id
-
+  cluster_id     = castai_aks_cluster.this[0].id
   name           = "default"
   disk_cpu_ratio = 0
   min_disk_size  = 100
@@ -60,41 +75,42 @@ resource "castai_node_configuration" "default" {
 
 resource "castai_node_configuration_default" "this" {
  count            = var.readonly ? 0 : 1
- cluster_id       = castai_aks_cluster.this.id
+ cluster_id       = castai_aks_cluster.this[0].id
  configuration_id = castai_node_configuration.default[0].id
 }
 
-resource "helm_release" "castai_cluster_controller" {
-  count            = var.readonly ? 0 : 1
-  name             = "cluster-controller"
-  repository       = "https://castai.github.io/helm-charts"
-  chart            = "castai-cluster-controller"
-  namespace        = "castai-agent"
-  create_namespace = true
-  cleanup_on_fail  = true
-  wait             = true
-
-  set {
-    name  = "castai.clusterID"
-    value = castai_aks_cluster.this.id
-  }
-
-  dynamic "set" {
-    for_each = var.castai_api_url != "" ? [var.castai_api_url] : []
-    content {
-      name  = "castai.apiURL"
-      value = var.castai_api_url
-    }
-  }
-
-  set_sensitive {
-    name  = "castai.apiKey"
-    value = var.castai_api_token
-  }
-
-  depends_on = [helm_release.castai_agent]
-
-  lifecycle {
-    ignore_changes = [version]
-  }
-}
+# Deploy phase 2 configuration
+# resource "helm_release" "castai_cluster_controller" {
+#   count            = var.readonly ? 0 : 1
+#   name             = "cluster-controller"
+#   repository       = "https://castai.github.io/helm-charts"
+#   chart            = "castai-cluster-controller"
+#   namespace        = "castai-agent"
+#   create_namespace = true
+#   cleanup_on_fail  = true
+#   wait             = true
+# 
+#   set {
+#     name  = "castai.clusterID"
+#     value = castai_aks_cluster.this.id
+#   }
+# 
+#   dynamic "set" {
+#     for_each = var.castai_api_url != "" ? [var.castai_api_url] : []
+#     content {
+#       name  = "castai.apiURL"
+#       value = var.castai_api_url
+#     }
+#   }
+# 
+#   set_sensitive {
+#     name  = "castai.apiKey"
+#     value = var.castai_api_token
+#   }
+# 
+#   depends_on = [helm_release.castai_agent]
+# 
+#   lifecycle {
+#     ignore_changes = [version]
+#   }
+# }
