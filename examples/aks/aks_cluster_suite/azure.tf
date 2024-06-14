0f3ab498e3c404b5c3bbecb1f0263f8bf1d2fb90
Elijah Abdulrahman <elijah@cast.ai>
2024-06-14 10:12:32 -0700

    cleanup: App registration is handled by module.castai

--- examples/aks/aks_cluster_suite/azure.tf
+++ examples/aks/aks_cluster_suite/azure.tf
@@ -7,60 +7,60 @@ data "azuread_client_config" "current" {}
 
 data "azurerm_subscription" "current" {}
 
-resource "azurerm_role_definition" "castai" {
-  name        = local.role_name
-  description = "Role used by CAST AI"
-
-  scope = "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group}"
-
-  permissions {
-    actions = [
-      "Microsoft.Compute/*/read",
-      "Microsoft.Compute/virtualMachines/*",
-      "Microsoft.Compute/virtualMachineScaleSets/*",
-      "Microsoft.Compute/disks/write",
-      "Microsoft.Compute/disks/delete",
-      "Microsoft.Compute/disks/beginGetAccess/action",
-      "Microsoft.Compute/galleries/write",
-      "Microsoft.Compute/galleries/delete",
-      "Microsoft.Compute/galleries/images/write",
-      "Microsoft.Compute/galleries/images/delete",
-      "Microsoft.Compute/galleries/images/versions/write",
-      "Microsoft.Compute/galleries/images/versions/delete",
-      "Microsoft.Compute/snapshots/write",
-      "Microsoft.Compute/snapshots/delete",
-      "Microsoft.Network/*/read",
-      "Microsoft.Network/networkInterfaces/write",
-      "Microsoft.Network/networkInterfaces/delete",
-      "Microsoft.Network/networkInterfaces/join/action",
-      "Microsoft.Network/networkSecurityGroups/join/action",
-      "Microsoft.Network/virtualNetworks/subnets/join/action",
-      "Microsoft.Network/applicationGateways/backendhealth/action",
-      "Microsoft.Network/applicationGateways/backendAddressPools/join/action",
-      "Microsoft.Network/applicationSecurityGroups/joinIpConfiguration/action",
-      "Microsoft.Network/loadBalancers/backendAddressPools/write",
-      "Microsoft.Network/loadBalancers/backendAddressPools/join/action",
-      "Microsoft.ContainerService/*/read",
-      "Microsoft.ContainerService/managedClusters/start/action",
-      "Microsoft.ContainerService/managedClusters/stop/action",
-      "Microsoft.ContainerService/managedClusters/runCommand/action",
-      "Microsoft.ContainerService/managedClusters/agentPools/*",
-      "Microsoft.Resources/*/read",
-      "Microsoft.Resources/tags/write",
-      "Microsoft.Authorization/locks/read",
-      "Microsoft.Authorization/roleAssignments/read",
-      "Microsoft.Authorization/roleDefinitions/read",
-      "Microsoft.ManagedIdentity/userAssignedIdentities/assign/action"
-    ]
-    not_actions = []
-  }
-
-  assignable_scopes = distinct(compact(flatten([
-    "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group}",
-    "/subscriptions/${var.subscription_id}/resourceGroups/${azurerm_kubernetes_cluster.this.node_resource_group}",
-    var.additional_resource_groups,
-  ])))
-}
+# resource "azurerm_role_definition" "castai" {
+#   name        = local.role_name
+#   description = "Role used by CAST AI"
+# 
+#   scope = "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group}"
+# 
+#   permissions {
+#     actions = [
+#       "Microsoft.Compute/*/read",
+#       "Microsoft.Compute/virtualMachines/*",
+#       "Microsoft.Compute/virtualMachineScaleSets/*",
+#       "Microsoft.Compute/disks/write",
+#       "Microsoft.Compute/disks/delete",
+#       "Microsoft.Compute/disks/beginGetAccess/action",
+#       "Microsoft.Compute/galleries/write",
+#       "Microsoft.Compute/galleries/delete",
+#       "Microsoft.Compute/galleries/images/write",
+#       "Microsoft.Compute/galleries/images/delete",
+#       "Microsoft.Compute/galleries/images/versions/write",
+#       "Microsoft.Compute/galleries/images/versions/delete",
+#       "Microsoft.Compute/snapshots/write",
+#       "Microsoft.Compute/snapshots/delete",
+#       "Microsoft.Network/*/read",
+#       "Microsoft.Network/networkInterfaces/write",
+#       "Microsoft.Network/networkInterfaces/delete",
+#       "Microsoft.Network/networkInterfaces/join/action",
+#       "Microsoft.Network/networkSecurityGroups/join/action",
+#       "Microsoft.Network/virtualNetworks/subnets/join/action",
+#       "Microsoft.Network/applicationGateways/backendhealth/action",
+#       "Microsoft.Network/applicationGateways/backendAddressPools/join/action",
+#       "Microsoft.Network/applicationSecurityGroups/joinIpConfiguration/action",
+#       "Microsoft.Network/loadBalancers/backendAddressPools/write",
+#       "Microsoft.Network/loadBalancers/backendAddressPools/join/action",
+#       "Microsoft.ContainerService/*/read",
+#       "Microsoft.ContainerService/managedClusters/start/action",
+#       "Microsoft.ContainerService/managedClusters/stop/action",
+#       "Microsoft.ContainerService/managedClusters/runCommand/action",
+#       "Microsoft.ContainerService/managedClusters/agentPools/*",
+#       "Microsoft.Resources/*/read",
+#       "Microsoft.Resources/tags/write",
+#       "Microsoft.Authorization/locks/read",
+#       "Microsoft.Authorization/roleAssignments/read",
+#       "Microsoft.Authorization/roleDefinitions/read",
+#       "Microsoft.ManagedIdentity/userAssignedIdentities/assign/action"
+#     ]
+#     not_actions = []
+#   }
+# 
+#   assignable_scopes = distinct(compact(flatten([
+#     "/subscriptions/${var.subscription_id}/resourceGroups/${var.resource_group}",
+#     "/subscriptions/${var.subscription_id}/resourceGroups/${azurerm_kubernetes_cluster.this.node_resource_group}",
+#     var.additional_resource_groups,
+#   ])))
+# }
 
 
 resource "azurerm_role_assignment" "castai_resource_group" {
