956cc42cf49ad5134d3428c9ea5d01cca6e4d962
Elijah Abdulrahman <elijah@cast.ai>
2024-10-03 11:54:02 -0700

    ref: azure provider update changed object/client_id

--- examples/aks/aks_cluster_suite/castai.tf
+++ examples/aks/aks_cluster_suite/castai.tf
@@ -71,7 +71,7 @@ resource "castai_aks_cluster" "this" {
   region          = var.cluster_region
   subscription_id = data.azurerm_subscription.current.subscription_id
   tenant_id       = data.azurerm_subscription.current.tenant_id
-  client_id       = var.readonly ? null : azuread_application.castai.application_id
+  client_id       = var.readonly ? null : azuread_application.castai.client_id
   client_secret   = var.readonly ? null : azuread_application_password.castai.value
 
   node_resource_group        = azurerm_kubernetes_cluster.this.node_resource_group
